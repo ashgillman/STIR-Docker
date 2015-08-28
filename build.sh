818f5ad3ace59d9e823c15e0a6b56a78c03dcc1f
Ashley Gillman <ashley.gillman@my.jcu.edu.au>
2015-08-28 14:56:11 +1000

    build.sh runs tests

--- build.sh
+++ build.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+
 docker build -t "stir:base" .
+
+if [ ! -d STIR/recon_test_pack ]; then
+  curl -O http://stir.sourceforge.net/registered/recon_test_pack.zip \
+       --netrc-file secrets.txt
+  unzip -a recon_test_pack.zip
+  rm recon_test_pack.zip
+fi
+
+docker run \
+       -v $PWD/STIR/recon_test_pack:/STIR/recon_test_pack \
+       -w /STIR/recon_test_pack/ \
+       stir:base \
+       ./run_tests.sh --mpicmd "mpirun -np $(getconf _NPROCESSORS_ONLN)"
